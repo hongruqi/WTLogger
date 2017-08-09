68bfc145eba9916d56cee6f5a04e9054d989d923
hongru qi <hongru.qi@quvideo.com>
2017-08-09 13:28:26 +0800

    Initial commit

---
+++ Example/WTLogger/WTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WTAppDelegate.h
+//  WTLogger
+//
+//  Created by lbrsilva-allin on 08/09/2017.
+//  Copyright (c) 2017 lbrsilva-allin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/WTLogger/WTViewController.h
@@ -0,0 +1,13 @@
+//
+//  WTViewController.h
+//  WTLogger
+//
+//  Created by lbrsilva-allin on 08/09/2017.
+//  Copyright (c) 2017 lbrsilva-allin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WTViewController : UIViewController
+
+@end
